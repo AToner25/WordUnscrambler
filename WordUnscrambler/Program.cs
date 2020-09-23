cd303bf04163bb1fb661432d7fe91b99ae52f144
Anthony Toner <1731452@champlaincollege.qc.ca>
2020-09-23 13:38:15 -0400

    I moved most of the strings to the class Constants and then used constants to call them.

--- WordUnscrambler/Program.cs
+++ WordUnscrambler/Program.cs
@@ -16,29 +16,46 @@ namespace WordUnscrambler
         {
             try
             {
-                Console.WriteLine("Enter scrambled word(s) manually or as a file: F - file / M - manual");
+                Console.WriteLine(Constants.FirstQuestion);
 
-                String option = Console.ReadLine() ?? throw new Exception("String is empty/null");
+                String option = Console.ReadLine() ?? throw new Exception(Constants.Null);
 
-                switch (option.ToUpper())
+                while (option.ToUpper() != "F" || option.ToUpper() == "M")
                 {
-                    case "F":
-                        Console.WriteLine("Enter file path including the file name: ");
-                        ExecuteScrambledWordsInFileScenario();
+                    switch (option.ToUpper())
+                    {
+                        case "F":
+                            Console.WriteLine(Constants.FileName);
+                            ExecuteScrambledWordsInFileScenario();
+                            break;
+                        case "M":
+                            Console.WriteLine(Constants.ManualWord);
+                            ExecuteScrambledWordManualEntryScenario();
+                            break;
+                        default:
+                            Console.WriteLine(Constants.OptionNotFound);
+                            break;
+                    }
+                }
+                Console.WriteLine(Constants.Continue);
+
+                string quit = Console.ReadLine() ?? throw new Exception(Constants.Null);
+
+                switch (quit.ToUpper())
+                {
+                    case "Y":
                         break;
-                    case "M":
-                        Console.WriteLine("Enter word(s) manually (separated by commas if mutiple)");
-                        ExecuteScrambledWordManualEntryScenario();
+                    case "N":
                         break;
-                    default:
-                        Console.WriteLine("The entered option was not recognized");
+                    case "YES":
+                        break;
+                    case "NO":
                         break;
                 }
-
             }
             catch (Exception ex)
             {
-                Console.WriteLine("The program will be terminated." + ex.Message);
+                Console.WriteLine(Constants.Terminate + ex.Message);
             }
         }
 
--- WordUnscrambler/WordMatcher.cs
+++ WordUnscrambler/WordMatcher.cs
@@ -12,45 +12,45 @@ namespace WordUnscrambler
         {
             List<MatchedWord> matchedWords = new List<MatchedWord>();
 
-            foreach (var scrambledWord in scrambledWords)
-            {
-                foreach (var word in wordList)
-                {
-                    //scrambled word already matches word
-                    if (scrambledWord.Equals(word, StringComparison.OrdinalIgnoreCase))
-                    {
-                        matchedWords.Add(BuildMatchedWord(scrambledWord, word));
-                    }
-                    else
-                    {
-                        //convert strings into char array
-                        char [] scrambledWordArray = scrambledWords.ToCharArray();
-                        char[] wordListArray = wordList.ToCharArray();
+            //foreach (var scrambledWord in scrambledWords)
+            //{
+            //    foreach (var word in wordList)
+            //    {
+            //        //scrambled word already matches word
+            //        if (scrambledWord.Equals(word, StringComparison.OrdinalIgnoreCase))
+            //        {
+            //            matchedWords.Add(BuildMatchedWord(scrambledWord, word));
+            //        }
+            //        else
+            //        {
+            //            //convert strings into char array
+            //            char [] scrambledWordArray = scrambledWords.ToCharArray();
+            //            char[] wordListArray = wordList.ToCharArray();
 
-                        //sort both character arrays (Array.sort())
-                        Array.Sort(scrambledWordArray);
-                        Array.Sort(wordListArray);
-                        //act -> sort -> act
-                        //cat -> sort -> act
+            //            //sort both character arrays (Array.sort())
+            //            Array.Sort(scrambledWordArray);
+            //            Array.Sort(wordListArray);
+            //            //act -> sort -> act
+            //            //cat -> sort -> act
 
-                        //compare sorted char arrays or convert to string and compare 
-                        scrambledWordArray.ToString();
-                        wordListArray.ToString();
+            //            //compare sorted char arrays or convert to string and compare 
+            //            scrambledWordArray.ToString();
+            //            wordListArray.ToString();
 
-                        foreach (var scrambledWord2 in scrambledWordArray)
-                        {
-                            foreach (var word2 in wordList)
-                            {
-                                //if they are equal, add to matchedWords list
-                                if (scrambledWord2.Equals(word2, StringComparison.OrdinalIgnoreCase))
-                                {
-                                    matchedWords.Add(BuildMatchedWord(scrambledWord2, word2));
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            //            foreach (var scrambledWord2 in scrambledWordArray)
+            //            {
+            //                foreach (var word2 in wordList)
+            //                {
+            //                    //if they are equal, add to matchedWords list
+            //                    if (scrambledWord2.Equals(word2, StringComparison.OrdinalIgnoreCase))
+            //                    {
+            //                        matchedWords.Add(BuildMatchedWord(scrambledWord2, word2));
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
 
             MatchedWord BuildMatchedWord(string scrambledWord, string word)
             {
